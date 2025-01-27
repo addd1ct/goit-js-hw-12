6c21981fc8b917172efa4c482c5133d5ec3c47b5
polly <polifibliss@gmail.com>
2025-01-27 14:19:58 +0200

    update

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,6 +1,3 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-
 export function renderImages(images) {
   const imagesList = document.querySelector('.images-container');
   
@@ -32,18 +29,4 @@ export function toggleLoadMoreButton(isVisible) {
   } else {
     loadMoreButton.classList.add('is-hidden');
   }
-}
-
-export function showNoResultsMessage() {
-      iziToast.show({
-        title: 'Notice',
-        message: "Sorry, there are no images matching your search query. Please try again.",
-    });
-}
-
-export function showEndOfResultsMessage() {
-    iziToast.show({
-        title: 'Notice',
-        message: "We're sorry, but you've reached the end of search results.",
-    });
 }
